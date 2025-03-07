63fd8d774ba63039b7b763668a7d4a7dfc68ad98
VVG <aston7061@yandex.ru>
2025-03-07 18:36:00 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -35,7 +35,7 @@ function error {
 _raw_base="https://raw.githubusercontent.com/GermanRuss/nginx-proxy-manager-lxc/refs/heads/main/"
 # Operating system
 _os_type=debian
-_os_version=12.3-7
+_os_version=12.3
 # System architecture
 _arch=$(dpkg --print-architecture)
 
